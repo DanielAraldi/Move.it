adc97e0116883b0bbe43365b7e2f605d9fde1f15
Daniel <danielsaraldi@gmail.com>
2021-03-25 20:46:40 -0300

    feat: see Hooks React concepts

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -3,9 +3,9 @@ import { Button } from "./components/Button";
 function App() {
   return (
     <div>
-      <Button color="red" />
-      <Button color="blue" />
-      <Button color="green" />
+      <Button color="red">Botão 1</Button>
+      <Button color="blue">Botão 2</Button>
+      <Button color="green">Botão 3</Button>
     </div>
   );
 }
--- moveit/src/components/Button.tsx
+++ moveit/src/components/Button.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 interface ButtonProps {
   color: string;
+  children: string;
 }
 
-export function Button({ color }: ButtonProps) {
+export function Button({ color, children }: ButtonProps) {
+  const [counter, setCounter] = useState(1);
+
+  const increment = () => setCounter(counter + 1);
+
   return (
-    <button type="button" style={{ backgroundColor: color }}>
-      {color}
+    <button
+      type="button"
+      style={{ backgroundColor: color }}
+      onClick={increment}
+    >
+      {children} - <strong>{counter}</strong>
     </button>
   );
 }
