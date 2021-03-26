d9025a785f24b3c35a1241810b57efb14e5d0b71
Daniel <danielsaraldi@gmail.com>
2021-03-25 21:49:59 -0300

    feat: create ExperienceBar Component

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,11 +1,11 @@
-import { Button } from "./components/Button";
+import { ExperienceBar } from "./components/ExperienceBar";
+
+import "./styles/global.css";
 
 function App() {
   return (
-    <div>
-      <Button color="red">Botão 1</Button>
-      <Button color="blue">Botão 2</Button>
-      <Button color="green">Botão 3</Button>
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
