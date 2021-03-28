2e6f4e240a3c3bdf150f43fadafa7875fd7e4e3c
Daniel <danielsaraldi@gmail.com>
2021-03-28 16:16:55 -0300

    feat: make CompletedChallenges dynamic

--- move-it/src/components/CompletedChallenges.tsx
+++ move-it/src/components/CompletedChallenges.tsx
@@ -1,10 +1,17 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengeContext";
+
 import styles from "../styles/components/CompletedChallenges.module.css";
 
 export function CompletedChallenges() {
+  const { challengeCompleted } = useContext(ChallengesContext);
+
   return (
     <div className={styles.completedChallengeContainer}>
-      <span>Desafios completos</span>
-      <span>5</span>
+      <span>
+        {challengeCompleted === 1 ? "Desafio completo" : "Desafios completos"}
+      </span>
+      <span>{challengeCompleted}</span>
     </div>
   );
 }
