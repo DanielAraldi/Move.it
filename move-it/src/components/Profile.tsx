38402df3153f2007f6b5e315c6e1799504d3f8c9
Daniel <danielsaraldi@gmail.com>
2021-03-29 20:04:35 -0300

    feat: component Profile dynamic

--- move-it/src/components/Profile.tsx
+++ move-it/src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengeContext";
+
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img
@@ -12,7 +17,7 @@ export function Profile() {
         <strong>Daniel Sansão Araldi</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
