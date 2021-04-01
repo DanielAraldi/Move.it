47c0e54c778cb840ef4a44953ff0c44db5568ff8
Daniel <danielsaraldi@gmail.com>
2021-03-31 21:17:39 -0300

    feat: update time and deploy project

--- move-it/src/contexts/CountdownContext.tsx
+++ move-it/src/contexts/CountdownContext.tsx
@@ -27,7 +27,7 @@ export const CountdownContext = createContext({} as CountdownContextData);
 export function CountdownProvider({ children }: CountdownContextProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -40,7 +40,7 @@ export function CountdownProvider({ children }: CountdownContextProps) {
     clearTimeout(countdownTimeout);
     setIsActive(false);
     setHasFinished(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
   };
 
   useEffect(() => {
